fe1d765e04220e09b86524a6a45f04a1dddd5504
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2023-08-18 18:21:08 -0500

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Ayden Wyer | Dashboard',
+  description: 'Welcome to my personal portfolio',
+}
+
+export default function page() {
+  return (
+    <div>dashboard</div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Ayden Wyer',
+  description: 'Welcome to my personal portfolio',
+}
+
+export default function Home() {
+  return (
+    <div>home</div>
+  )
+}
