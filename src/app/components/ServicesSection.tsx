c560f32082ad20acfe6c59ed96d9c0c2b12baec3
Ayden Wyer <aydenreese@hotmail.com>
2023-12-18 17:40:45 -0600

    Updating footer

--- src/app/components/ServicesSection.tsx
+++ src/app/components/ServicesSection.tsx
@@ -6,11 +6,13 @@ const ServicesSection = () => {
   return (
     <section id='services'>
         <Heading preheader="services" header="What I can do for you" paragraph="I offer a variety of services that will help give your business the visual identity it deserves."/>
+        <div className="flex flex-col gap-2">
         {
             Services.map((service, idx) => (
                 <ServiceCard key={idx} heading={service.serviceName} paragraph={service.serviceDescription}/>
             ))
         }
+        </div>
     </section>
   )
 }
--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full p-5 bg-[#202020] border-neutral-800 border-t-[1px] text-center font-regular text-xs">
+    <footer className="mt-auto w-full p-5 bg-[#202020] border-neutral-800 border-t-[1px] text-center font-regular text-xs">
         &copy; {new Date().getFullYear()} Ayden Wyer. All Rights Reserved.
     </footer>
   )
