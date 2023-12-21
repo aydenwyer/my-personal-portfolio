61b3e3db5e27d81a66461b7cc0c59efefe856682
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2023-12-21 12:10:34 -0600

    Reseting form fields after submit

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button() {
     <button type="submit" className='flex gap-2 items-center justify-center py-1 px-4 hover:text-white rounded-sm active-nav-link border-[1px] font-medium text-md hover:gap-3 hover:bg-neutral-700 transition-all ease-out duration-150 disabled:gap-2'
     disabled={pending}
     >
-        {pending ? <div className='h-7 w-7 animate-spin motion-reduce:animate-spin rounded-full border-b-2 border-white'></div> : <>
+        {pending ? <div className='h-5 w-5 animate-spin motion-reduce:animate-spin rounded-full border-b-2 border-white'></div> : <>
             Submit
             <Image src="/arrow-right.svg" width={12} height={7} alt="Arrow Right" />
         </>}
--- src/app/components/sections/contact/ContactForm.tsx
+++ src/app/components/sections/contact/ContactForm.tsx
@@ -2,9 +2,20 @@
 
 import { sendEmail } from "@/actions/sendEmail";
 import toast from "react-hot-toast";
-import SubmitBtn from '../../Button'
+import SubmitBtn from '../../Button';
+import {useState} from 'react';
 
 const ContactForm = () => {
+  const handleSubmit = () => {
+    setFormValues({name: "", email: "", message: ""})
+  }
+
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    message: ""
+  })
+
   return (
     <form
       className="flex flex-col gap-2"
@@ -18,8 +29,8 @@ const ContactForm = () => {
         }
 
         toast.success("Email sent successfully");
-
       }}
+      onSubmit={handleSubmit}
     >
       <div className="grid grid-flow-col grid-cols-2 gap-3">
         <div className="flex flex-col gap-1">
@@ -30,6 +41,8 @@ const ContactForm = () => {
             id="sender-name"
             maxLength={30}
             className="bg-white/[.03] rounded-sm border-[1px] border-transparent hover:bg-white/[.06] hover:border-[#424242] focus:bg-white/[.06] focus:border-[#838383] active:outline-none focus:outline-none transition-all duration-150 px-3 py-2"
+            onChange={(e) => setFormValues({...formValues, name: e.target.value})}
+            value={formValues.name}
           />
         </div>
         <div className="flex flex-col gap-1">
@@ -41,6 +54,8 @@ const ContactForm = () => {
             required
             maxLength={100}
             className="bg-white/[.03] rounded-sm border-[1px] border-transparent hover:bg-white/[.06] hover:border-[#424242] focus:bg-white/[.06] focus:border-[#838383] active:outline-none focus:outline-none transition-all duration-150 px-3 py-2"
+            onChange={(e) => setFormValues({...formValues, email: e.target.value})}
+            value={formValues.email}
           />
         </div>
       </div>
@@ -52,6 +67,8 @@ const ContactForm = () => {
           required
           rows={10}
           className="bg-white/[.03] rounded-sm border-[1px] border-transparent hover:bg-white/[.06] hover:border-[#424242] focus:bg-white/[.06] focus:border-[#838383] active:outline-none focus:outline-none transition-all duration-150 px-3 py-2"
+          onChange={(e) => setFormValues({...formValues, message: e.target.value})}
+          value={formValues.message}
         />
       </div>
       <SubmitBtn />
