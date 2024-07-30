370c9f1b657e8d2f3991d2139e9cae59226d578f
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2024-07-30 15:53:42 -0500

    Changing projects

--- src/lib/_projects.ts
+++ src/lib/_projects.ts
@@ -1,26 +1,12 @@
 const Projects = [
-    {
-        image: "/box-shadow-image.png",
-        projectName: "Box Shadow Visualizer",
-        projectDesc: "Visualize your CSS box shadow with easy to use sliders and color inputs. After you’re happy with the result, export the code and use it in your current project!",
-        tech: ["JavaScript"],
-        link: "https://aydenwyer.github.io/box-shadow-generator/"
-    },
-    {
-        image: "/box-shadow-image.png",
-        projectName: "Box Shadow Visualizer",
-        projectDesc: "Visualize your CSS box shadow with easy to use sliders and color inputs. After you’re happy with the result, export the code and use it in your current project!",
-        tech: ["JavaScript"],
-        link: "https://aydenwyer.github.io/box-shadow-generator/"
-    },
-    {
-        image: "/box-shadow-image.png",
-        projectName: "Box Shadow Visualizer",
-        projectDesc: "Visualize your CSS box shadow with easy to use sliders and color inputs. After you’re happy with the result, export the code and use it in your current project!",
-        tech: ["JavaScript"],
-        link: "https://aydenwyer.github.io/box-shadow-generator/"
-    },
-
-]
+  {
+    image: "/box-shadow-image.png",
+    projectName: "Box Shadow Visualizer",
+    projectDesc:
+      "Visualize your CSS box shadow with easy to use sliders and color inputs. After you’re happy with the result, export the code and use it in your current project!",
+    tech: ["JavaScript"],
+    link: "https://aydenwyer.github.io/box-shadow-generator/",
+  },
+];
 
 export default Projects;
