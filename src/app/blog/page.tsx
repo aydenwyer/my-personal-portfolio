cf64568d0fde68febf3e6911f725d9e697fa7950
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2024-07-30 15:45:36 -0500

    Fixing deployment errors

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,15 @@
-import SectionHeading from "../components/SectionHeading"
-import BlogPostCard from "./BlogPostCard"
-
+import SectionHeading from "@/components/SectionHeading";
+import BlogPostCard from "./BlogPostCard";
 
 export default function blog() {
   return (
     <section className="flex flex-col gap-44">
-      <SectionHeading preheader="blog" header="Learn from me" paragraph="Here are a list of blog posts that I've written to capture my thoughts, and teach what I know."/>
-      <div>
-        
-      </div>
+      <SectionHeading
+        preheader="blog"
+        header="Learn from me"
+        paragraph="Here are a list of blog posts that I've written to capture my thoughts, and teach what I know."
+      />
+      <div></div>
     </section>
-  )
+  );
 }
