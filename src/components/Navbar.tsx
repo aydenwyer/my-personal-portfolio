dbaa56713616a7c8656c8526eab6d095ddaf1905
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2024-07-31 22:45:13 -0500

    Turning off scroll when navbar is open

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import NavNameTag from "@/components/NavNameTag";
 import NavLinkList from "@/components/NavLinkList";
 import NavIcons from "@/components/NavIcons";
@@ -11,26 +11,44 @@ import clsx from "clsx";
 const Navbar = () => {
   const { navOpen, setNavOpen } = useNavbarStateContext();
 
-  return (
-    <aside
-      className={clsx(
-        "fixed top-0 h-screen flex justify-between lg:static bg-[#202020] border-r-[1px] border-[#292929] w-72 px-4 py-6 flex-col z-50 transition-all duration-300 ease-in-out",
-        navOpen ? "right-0" : "-right-72"
-      )}
-    >
-      <div className="flex flex-col">
-        <NavNameTag className="hidden lg:flex" />
-        <div
-          onClick={() => setNavOpen(false)}
-          className="lg:hidden p-3 self-end cursor-pointer rounded-md border-[1px] border-[#292929]"
-        >
-          <X />
-        </div>
-        <NavLinkList />
-      </div>
+  useEffect(() => {
+    const stopScroll = () => {
+      document.body.classList.add("stop-scroll");
+    };
 
-      <NavIcons />
-    </aside>
+    const startScroll = () => {
+      document.body.classList.remove("stop-scroll");
+    };
+
+    if (navOpen) {
+      stopScroll();
+    } else {
+      startScroll();
+    }
+  }, [navOpen, setNavOpen]);
+
+  return (
+    <>
+      <aside
+        className={clsx(
+          "fixed top-0 h-screen flex justify-between lg:static bg-[#202020] border-r-[1px] border-[#292929] w-72 px-4 py-6 flex-col z-50 transition-all duration-300 ease-in-out",
+          navOpen ? "right-0" : "-right-72"
+        )}
+      >
+        <div className="flex flex-col">
+          <NavNameTag className="hidden lg:flex" />
+          <div
+            onClick={() => setNavOpen(false)}
+            className="lg:hidden p-3 self-end cursor-pointer rounded-md border-[1px] border-[#292929]"
+          >
+            <X />
+          </div>
+          <NavLinkList />
+        </div>
+
+        <NavIcons />
+      </aside>
+    </>
   );
 };
 
