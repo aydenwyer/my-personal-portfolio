2b5280c4ee51c057762ee06db2fdf9c9d28a549e
Ayden <aydenreese@hotmail.com>
2024-08-01 17:37:58 -0500

    Fixing errors

--- src/app/project/[name]/page.tsx
+++ src/app/project/[name]/page.tsx
@@ -1,10 +1,9 @@
-import { promises as fs } from "fs";
+import fs from "fs";
+import path from 'path'
 
 const page = async ({ params }: { params: { name: string } }) => {
-  const file = await fs.readFile(
-    process.cwd() + `/src/lib/projects/${params.name}.json`,
-    "utf-8"
-  );
+  let projectPath = path.join(process.cwd(), `/src/lib/projects/${params.name}.json`)
+  let file = fs.readFileSync(projectPath, 'utf-8')
   const data = JSON.parse(file);
 
   return (
@@ -13,19 +12,19 @@ const page = async ({ params }: { params: { name: string } }) => {
         <h3 className="text-sm uppercase tracking-[.2em] text-[#666666]">
           {data.company}
         </h3>
-        <h1 className="font-semibold text-2xl bg-gradient-to-r from-white to-[#bdbdbd] inline-block text-transparent bg-clip-text leading-none mb-3 sm:text-3xl sm:leading-snug sm:py-0">
+        <h1 className="font-bold text-2xl bg-gradient-to-r from-white to-[#bdbdbd] inline-block text-transparent bg-clip-text leading-none mb-3 sm:text-3xl sm:leading-snug sm:py-0">
           {data.name}
         </h1>
       </div>
 
       <div>
-        <h2 className="font-semibold text-lg text-[#dadada] inline-block leading-none mb-3 sm:text-xl sm:leading-snug sm:py-0">
+        <h2 className="font-medium text-lg text-[#dadada] inline-block leading-none mb-3 sm:text-xl sm:leading-snug sm:py-0">
           Problem
         </h2>
         <p>{data.problem}</p>
       </div>
       <div>
-        <h2 className="font-semibold text-lg text-[#dadada] inline-block leading-none mb-3 sm:text-xl sm:leading-snug sm:py-0">
+        <h2 className="font-medium text-lg text-[#dadada] inline-block leading-none mb-3 sm:text-xl sm:leading-snug sm:py-0">
           Solution
         </h2>
         <p>{data.ideation}</p>
